36b8c47f8dcb8358f41d5c044d86307587223c4b
Srinivasulu Thatha <thatha.srinivasulu@gmail.com>
2019-12-08 16:06:18 +0530

    Assignment Two Submission

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    private String message;
+
+    public WelcomeController(
+            // Injects the WELCOME_MESSAGE environment variable
+            @Value("${welcome.message}") String message
+    ) {
+        this.message = message;
+    }
+
     @GetMapping("/")
-    public String sayHello(){
-        return "hello";
+    public String sayHello() {
+        return message;
     }
 }
