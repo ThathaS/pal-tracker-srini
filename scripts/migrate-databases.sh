d0f398bd4201f7bb3e8c1d8aaab917c4ec5c695e
Srinivasulu Thatha <thatha.srinivasulu@gmail.com>
2019-12-12 05:40:51 +0530

    Add task for migrating databases
    # Conflicts:
    #	.travis.yml

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-app_name="${1:-pal-tracker}"
+app_name="${1:-pal-tracker-srini}"
 script_dir="${2:-.}"
-service_name="${3:-tracker-database}"
+service_name="${3:-pal-tracker-srini-db}"
 service_key="${4:-flyway-migration-key}"
 
 function main() {
